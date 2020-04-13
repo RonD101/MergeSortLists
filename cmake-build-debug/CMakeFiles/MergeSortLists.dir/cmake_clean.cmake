f86d0625d7b201d946d33eb2451cb817942460b3
rond <ron.dahan@campus.technion.ac.il>
2020-04-13 18:34:44 +0300

    PLEASE

---
+++ cmake-build-debug/CMakeFiles/MergeSortLists.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MergeSortLists.dir/main.c.o"
+  "MergeSortLists"
+  "MergeSortLists.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MergeSortLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
