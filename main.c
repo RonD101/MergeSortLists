ae94508dbf08ed2d82b106ccf8e9073531fb4c45
yoav <kenkendler123@gmail.com>
2020-04-13 19:34:18 +0300

    cmon!

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "MergeList.h"
 
 int main() {
-    printf("Hello, World!\n");
+    Node list = malloc(sizeof(Node));
+    list->x = 1;
+    list->next = malloc(sizeof(Node));
+    list->next->x = 2;
+    list->next->next = NULL;
+    printf("%d %d", list->x, list->next->x);
+
+    printf("\ntest");
+
+    free(list->next);
+
+    printf("\ntest2");
+
+    free(list);
     return 0;
 }
