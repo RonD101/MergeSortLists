4112c5dad643d9abfb2f0e13bf965726b5e0afd4
rond <ron.dahan@campus.technion.ac.il>
2020-04-13 21:25:41 +0300

    PLEASE

--- MergeList.c
+++ MergeList.c
@@ -8,66 +8,77 @@
 #include <stdlib.h>
 
 ErrorCode mergeSortedLists(Node list1,Node list2,Node *merge_out) {
+    *merge_out = malloc(sizeof(Node));/////
+    Node merge_out_tmp = *merge_out;
+    (*merge_out)->next = NULL;
     if(!list1||!list2)
         return EMPTY_LIST;
     if(!(*merge_out))
         return NULL_ARGUMENT;
     if(!(isListSorted(list1)) || !(isListSorted(list2)))
         return UNSORTED_LIST;
-
+    bool first_run = true;
     while (list1 != NULL && list2 != NULL) {
-
+        if(!first_run)
+            (*merge_out)->next = malloc(sizeof(merge_out));
+        first_run =false;
         if (list1->x < list2->x) {
             (*merge_out)->x = list1->x;
-            (*merge_out)->next = malloc(sizeof(merge_out));
+            //(*merge_out)->next = malloc(sizeof(merge_out));
             if((*merge_out)->next == NULL){
                 freeMemory(*merge_out);
                 *merge_out = NULL;
                 return MEMORY_ERROR;
             }
             *merge_out = (*merge_out)->next;
+            //(*merge_out)->next = NULL;///////////
             list1 = list1->next;
         } else {
             (*merge_out)->x = list2->x;
-            (*merge_out)->next = malloc(sizeof(merge_out));
+            //(*merge_out)->next = malloc(sizeof(merge_out));
             if((*merge_out)->next == NULL){
                 freeMemory(*merge_out);
                 *merge_out = NULL;
                 return MEMORY_ERROR;
             }
             *merge_out = (*merge_out)->next;
+            //(*merge_out)->next = NULL;///////////
             list2 = list2->next;
         }
     }
     while (list1) {
         (*merge_out)->x = list1->x;
-        (*merge_out)->next = malloc(sizeof(merge_out));
+        //(*merge_out)->next = malloc(sizeof(merge_out));
         if((*merge_out)->next == NULL){
             freeMemory(*merge_out);
             *merge_out = NULL;
             return MEMORY_ERROR;
         }
         *merge_out = (*merge_out)->next;
+        //(*merge_out)->next = NULL;///////////
         list1 = list1->next;
     }
     while (list2) {
         (*merge_out)->x = list2->x;
-        (*merge_out)->next = malloc(sizeof(merge_out));
+        //(*merge_out)->next = malloc(sizeof(merge_out));
         if((*merge_out)->next == NULL){
             freeMemory(*merge_out);
             *merge_out = NULL;
             return MEMORY_ERROR;
         }
         *merge_out = (*merge_out)->next;
+        //(*merge_out)->next = NULL;///////////
         list2 = list2->next;
     }
+    *merge_out = merge_out_tmp;
+
     return SUCCESS;
 }
 
 int getListLength(Node list)
 {
     int counter = 0;
-    while(list->next != NULL)
+    while(list != NULL)
     {
         counter++;
         list = list->next;
@@ -81,8 +92,10 @@ bool isListSorted(Node list)
 {
     while(list->next != NULL)
     {
-        if(list->x >= list->next->x)
+        if(list->x >= list->next->x) {
             return false;
+        }
+        list = list->next;
     }
     return true;
 }
--- main.c
+++ main.c
@@ -6,16 +6,36 @@ int main() {
     Node list = malloc(sizeof(Node));
     list->x = 1;
     list->next = malloc(sizeof(Node));
-    list->next->x = 2;
+    list->next->x = 13;
     list->next->next = NULL;
-    printf("%d %d", list->x, list->next->x);
+    Node list2 = malloc(sizeof(Node));
+    list2->x = 2;
+    list2->next = malloc(sizeof(Node));
+    list2->next->x = 15;
+    list2->next->next = NULL;
+    Node list3 = NULL;//malloc(sizeof(Node));
 
-    printf("\ntest");
+    int x = mergeSortedLists(list,list2,&list3);
 
+    printf("%d\n",x);
+    while ((list3)){
+        printf("%d ",list3->x);
+        list3 = list3->next;
+    }
+   // printf("\n%d-%d-%d-%d",list3->x,list3->next->x,list3->next->next->x,list3->next->next->next->x);
+    /*printf("%d %d", list->x, list->next->x);
+    printf("\n%d",getListLength(list));
+    int i = isListSorted(list);
+    printf("\n%d",i);
+    printf("\ntest");*/
+
+
+
+    //printf("\ntest2");
     free(list->next);
-
-    printf("\ntest2");
-
     free(list);
+    free(list2->next);
+    free(list2);
+    free(list3);
     return 0;
 }
