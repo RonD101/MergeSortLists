547c4dc008a4b63b24aed8ea3a8aab98ed1f04c9
rond <ron.dahan@campus.technion.ac.il>
2020-04-13 18:48:21 +0300

    PLEASE

--- MergeList.c
+++ MergeList.c
@@ -2,5 +2,68 @@
 // Created by Ron Dahan on 13/04/2020.
 //
 
-cal
+#include <stdio.h>
+#include <stdbool.h>
+#include "MergeList.h"
+#include <stdlib.h>
+
+ErrorCode mergeSortedLists(Node list1,Node list2,Node *merge_out) {
+
+
+    while (list1 != NULL && list2 != NULL) {
+
+        if (list1->x < list2->x) {
+            (*merge_out)->x = list1->x;
+            (*merge_out)->next = malloc(sizeof((*merge_out)->next));
+            /////null
+            merge_out = (*merge_out)->next;
+            list1 = list1->next;
+        } else {
+            (*merge_out)->x = list2->x;
+            (*merge_out)->next = malloc(sizeof((*merge_out)->next));
+            /////null
+            merge_out = (*merge_out)->next;
+            list2 = list2->next;
+        }
+    }
+    while (list1) {
+        (*merge_out)->x = list1->x;
+        (*merge_out)->next = malloc(sizeof((*merge_out)->next));
+        /////null
+        merge_out = (*merge_out)->next;
+        list1 = list1->next;
+    }
+    while (list2) {
+        (*merge_out)->x = list2->x;
+        (*merge_out)->next = malloc(sizeof((*merge_out)->next));
+        /////null
+        merge_out = (*merge_out)->next;
+        list2 = list2->next;
+    }
+    return MEMORY_ERROR;
+}
+
+int getListLength(Node list)
+{
+    int counter = 0;
+    while(list->next != NULL)
+    {
+        counter++;
+        list = list->next;
+    }
+    if(counter == 0)
+        return counter;
+}
+
+
+
+bool isListSorted(Node list)
+{
+    while(list->next != NULL)
+    {
+        if(list->x >= list->next->x)
+            return false;
+    }
+    return true;
+}
 
--- MergeList.h
+++ MergeList.h
@@ -1,8 +1,20 @@
 //
 // Created by Ron Dahan on 13/04/2020.
 //
+#include <stdbool.h>
 
-#ifndef MERGESORTLISTS_MERGELIST_H
-#define MERGESORTLISTS_MERGELIST_H
+#ifndef MERGESORTEDLIST_MERGELIST_H
+#define MERGESORTEDLIST_MERGELIST_H
 
-#endif //MERGESORTLISTS_MERGELIST_H
+typedef struct node_t {
+    int x;
+    struct node_t *next;
+} *Node;
+
+typedef enum { SUCCESS=0,
+    MEMORY_ERROR, EMPTY_LIST , UNSORTED_LIST, NULL_ARGUMENT ,
+} ErrorCode;
+int getListLength(Node list);
+bool isListSorted(Node list);
+ErrorCode mergeSortedLists(Node list1, Node list2, Node *merged_out);
+#endif //MERGESORTEDLIST_MERGELIST_H
